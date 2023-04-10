00ab15c86772c322daf869a4dd2805d0808cde6a
Your Name <you@example.com>
2023-04-11 00:38:12 +0300

    "Вт 11 апр 2023 00:38:12" -

--- promtstd/__main__.py
+++ promtstd/__main__.py
@@ -11,61 +11,73 @@ from lib_to_json import ParserPromtStdToJson
 
 
 def main(to_build: Literal["json", "html"] = None, file: Optional[str] = None):
-    file_md = None
+    file_out = None
     if not file:
         parser = ArgumentParser(
             description="Преобразование файла PromtStd.Markdown в формат PromtStd.JSON"
         )
-        parser.add_argument("to_build", type=str, help="В какой тип нужно собрать")
-        parser.add_argument("file_md", type=str, help="Путь к PromtStd.Markdown")
+        parser.add_argument(
+            "to_build",
+            choices=["json", "html"],
+            type=str,
+            help="В какой тип нужно собрать",
+        )
+        parser.add_argument(
+            "file_md",
+            type=str,
+            help="Путь к PromtStd.Markdown",
+        )
 
         args = parser.parse_args()
-        file_md = Path(args.file_md)
+        to_build = args.to_build
+        file_out = Path(args.file_md)
 
-        if file_md.suffix != ".md":
-            logLinter(StatusError.file_no_suffix_md, where_event=file_md)
+        if file_out.suffix != ".md":
+            logLinter(StatusError.file_no_suffix_md, where_event=file_out)
             raise LintError(StatusError.file_no_suffix_md.value)
 
     else:
-        file_md = Path(file)
+        file_out = Path(file)
 
     ###
     if to_build == "json":
-        r = ParserPromtStdToJson(file_md).parse_file_promt_md_to_json()
-        logLinter(StatusSuccess.end_compilation_json, where_event=file_md)
+        r = ParserPromtStdToJson(file_out).parse_file_promt_md_to_json()
+        logLinter(StatusSuccess.end_compilation_json, where_event=file_out)
 
-        file_md = file_md.parent / f"_{file_md.name}.json"
+        path_out_file_promt = (
+            file_out.parent / "json" / file_out.with_suffix(".json").name
+        )
 
-        path_out_file_promt = file_md
-
+        path_out_file_promt.parent.mkdir(exist_ok=True)
         path_out_file_promt.write_text(r.text)
-        logLinter(StatusSuccess.save_to_json_file, where_event=file_md)
+        logLinter(StatusSuccess.save_to_json_file, where_event=file_out)
 
     elif to_build == "html":
-        r = ParserPromtStdToHTML(file_md).parse_file_promt_md_to_html()
-        logLinter(StatusSuccess.end_compilation_html, where_event=file_md)
+        r = ParserPromtStdToHTML(file_out).parse_file_promt_md_to_html()
+        logLinter(StatusSuccess.end_compilation_html, where_event=file_out)
 
-        file_md = file_md.parent / f"_{file_md.name}.html"
+        path_out_file_promt = (
+            file_out.parent / "html" / file_out.with_suffix(".html").name
+        )
 
-        path_out_file_promt = file_md
-
+        path_out_file_promt.parent.mkdir(exist_ok=True)
         path_out_file_promt.write_text(r.text)
-        logLinter(StatusSuccess.save_to_json_html, where_event=file_md)
+        logLinter(StatusSuccess.save_to_json_html, where_event=file_out)
 
     else:
-        logLinter(StatusError.not_allowed_type_to_build, where_event=file_md)
+        logLinter(StatusError.not_allowed_type_to_build, where_event=file_out)
         raise LintError(StatusError.not_allowed_type_to_build.value)
 
 
 if __name__ == "__main__":
-    # main()
+    main()
 
-    to_build = "html"
-    pt = Path(__file__).parent.parent / "examples"
-    main(to_build, pt / "Генерация кода на Python.md")
-    main(to_build, pt / "Документирование кода.md")
-    main(to_build, pt / "Изучение новой темы в IT.md")
-    main(to_build, pt / "Написание README большому проекту.md")
-    main(to_build, pt / "Оптимизация кода.md")
-    main(to_build, pt / "Написать программу на Python.md")
-    main(to_build, pt / "Создание промтов вместе с ChatGpt.md")
+    # to_build = "json"
+    # pt = Path(__file__).parent.parent / "examples"
+    # main(to_build, pt / "Генерация кода на Python.md")
+    # main(to_build, pt / "Документирование кода.md")
+    # main(to_build, pt / "Изучение новой темы в IT.md")
+    # main(to_build, pt / "Написание README большому проекту.md")
+    # main(to_build, pt / "Оптимизация кода.md")
+    # main(to_build, pt / "Написать программу на Python.md")
+    # main(to_build, pt / "Создание промтов вместе с ChatGpt.md")
